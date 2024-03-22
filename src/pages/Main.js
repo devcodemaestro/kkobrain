6d2c8beac7a0991ba289b22f0359113f5e637096
devcodemaestro <dev.codemaestro@gmail.com>
2024-03-22 16:05:17 +0900

    Swiper 작업 진행 중

---
+++ src/pages/Main.js
@@ -0,0 +1,15 @@
+import "../css/main/main.css";
+
+import { MainBottom } from "../components/main/MainBottom";
+import { MainTop } from "../components/main/MainTop";
+
+const Main = () => {
+  return (
+    <>
+      <MainTop />
+      <MainBottom />
+    </>
+  );
+};
+
+export default Main;
