4616af0169be669200f8934617abc3c1f5f87cfa
devcodemaestro <dev.codemaestro@gmail.com>
2024-03-26 12:33:35 +0900

    네이버 서치어드바이저 업데이트

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,14 +5,19 @@ import { Metadata } from "next";
 export const metadata: Metadata = {
   title: "카카오 브레인 블로그 - 클론코드",
   description: "클론 코드를 통한 프로젝트",
-  themeColor: "#000000",
+  themeColor: "#181414",
   viewport: { width: "device-width", initialScale: 1 },
 };
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
     <html lang="ko">
-      <head></head>
+      <head>
+        <meta
+          name="naver-site-verification"
+          content="dc2618b11b331c780132c9c7f87e05b649942f6e"
+        />
+      </head>
       <body>
         <noscript>You need to enable JavaScript to run this app.</noscript>
         <div id="root">{children}</div>
